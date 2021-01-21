d29048a6bc4674a77921475ac976c7ed5891c33f
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-01-21 13:50:42 +0100

    logic done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [values, setValues] = useState(
+    {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+  );
+
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleFirstNameInputOnchange = (e) => {
+    setValues({...values, firstName: e.target.value});
+  };
+  const handleLastNameInputOnchange = (e) => {
+    setValues({...values, lastName: e.target.value});
+  };
+  const handleEmailInputOnchange = (e) => {
+    setValues({...values, email: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(values.firstName && values.lastName && values.email) {
+      setValid(true)
+    };
+    setSubmitted(true)
+  }
+
   return (
     <div className="App">
-      <form className="form-wrapper">
-        <div>Thank you for registering!</div>
-        <input type="text" placeholder='Enter First Name'></input>
-        <span>Please enter your first name</span>
-        <input type="text" placeholder='Enter Last Name'></input>
-        <span>Please enter your last name</span>
-        <input type="text" placeholder='Enter Email'></input>
-        <span>Please enter your email</span>
+      <form className="form-wrapper" onSubmit={handleSubmit}>
+        {submitted && valid ? <div>Thank you for registering!</div> : null}
+       
+        <input onChange={handleFirstNameInputOnchange} value={values.firstName} type="text" placeholder='Enter First Name'></input>
+        {submitted && !values.firstName ? <span>Please enter your first name</span> : null}
+        <input onChange={handleLastNameInputOnchange} value={values.lastName} type="text" placeholder='Enter Last Name'></input>
+        {submitted && !values.lastName ? <span>Please enter your last name</span> : null}
+        <input onChange={handleEmailInputOnchange} value={values.email} type="text" placeholder='Enter Email'></input>
+        {submitted && !values.email ? <span>Please enter your email</span> : null}
         <button type="submit">Submit</button>
       </form>
     </div>
